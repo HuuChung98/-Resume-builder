f11f63c27d6ed18272a321dc490cfc54368038c9
HuuChung98 <balechung93@gmail.com>
2023-11-12 23:15:23 +0700

    first commit

--- app/page.tsx
+++ app/page.tsx
@@ -108,6 +108,19 @@ export default function Home() {
           </p>
         </a>
       </div>
+      {/* ###################### */}
+      <div className="flex-1 w-full flex flex-col items-center">
+        <nav className="w-full flex justify-center border-b border-b-foreground/10 h-16">
+          <div className="w-full max-w-6xl flex justify-between items-center p-3 text-sm">
+            <span className="font-bold select-none">resumeGPT.</span>
+          </div>
+        </nav>
+
+        <div className="animate-in flex-1 flex flex-col opacity-0 max-w-6xl px-3">
+          <Home />
+        </div>
+      </div>
+      {/* ###################### */}
     </main>
   )
 }
